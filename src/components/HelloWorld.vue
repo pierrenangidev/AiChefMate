cce14edc8715c5becb704478ac8d1c613e3af126
Pierre nangi <pierre.nangi@outlook.com>
2025-09-05 21:23:26 +0200

    "Setup completed, now link local git to github repo"

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup>
+//import { ref } from 'vue'
+
+/*
+defineProps({
+  msg: String,
+})
+*/
+
+//const count = ref(0)
+</script>
+
+<template>
+  Test setuped !
+</template>
+
+<style scoped>
+
+</style>

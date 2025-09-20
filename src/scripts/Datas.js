13df04c178c447bfcb164367e9484eebb27b1930
Pierre nangi <pierre.nangi@outlook.com>
2025-09-20 16:39:44 +0200

    All Medias imported, integrated, Datas & Images scripts are created and finalised to be push in online repo

--- src/scripts/Datas.js
+++ src/scripts/Datas.js
@@ -1,23 +1,312 @@
+import { Images } from './images.js'
+
 export const Data = {
     content: {
+        navbar: {
+            logo: Images.navbar.logo,
+            logo2x: Images.navbar.logo2x,
+        },
         header: {
-            title: "Bienvenue sur AiChefMate",
-            subtitle: "Votre assistant culinaire intelligent"
+            title: "Elevate Your Mealtime with AI-Powered Personalization",
+            subtitle: "Effortless Planning, Healthier Eating",
+            images: {
+                bowl: Images.header.bowl,
+                bowl2x: Images.header.bowl2x,
+                groceryBag: Images.header.groceryBag,
+                groceryBag2x: Images.header.groceryBag2x,
+                natureHand: Images.header.natureHand,
+                natureHand2x: Images.header.natureHand2x,
+                vegetables: Images.header.vegetables,
+                vegetables2x: Images.header.vegetables2x,
+                icons: {
+                    bread: Images.header.icons.bread,
+                    bread2x: Images.header.icons.bread2x,
+                    carrot: Images.header.icons.carrot,
+                    carrot2x: Images.header.icons.carrot2x,
+                    cart: Images.header.icons.cart,
+                    cart2x: Images.header.icons.cart2x,
+                    cherry: Images.header.icons.cherry,
+                    cherry2x: Images.header.icons.cherry2x,
+                }
+            },
         },
         main: {
             sections: {
-                recipes: { title: "Recettes", content: "Découvrez nos délicieuses recettes." },
-                cookingTips: { title: "Conseils de cuisine", content: "Améliorez vos compétences culinaires." }
+                demo: { 
+                    title: "Demo",
+                    subtitle: "See how it works: your personalized meal planning journey",
+                    content: {
+                        description: "Every meal is a chance to nourish your body, inspire your mind, and feed your soul.",
+                        images: {
+                            demo: Images.main.demo.demo,
+                            demo2x: Images.main.demo.demo2x,
+                            player: Images.main.demo.player,
+                            player2x: Images.main.demo.player2x,
+                        }
+                    }
+                },
+                howItWorks: { 
+                    title: "How It Works",
+                    subtitle: "Step into the world of hassle-free meal planning with our easy 3-step process",
+                    content: {
+                        step1: {
+                            numberTitle: "01",
+                            title: "Personalize Your Profile",
+                            content: "Begin by creating your profile. Tell us about your dietary preferences, nutritional goals, and budget. Our AI technology tailors every meal plan to fit your unique needs."
+                        },
+                        step2: {
+                            numberTitle: "02",
+                            title: "Customize Your Plan",
+                            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod, nisi eu consectetur cursus, nisl erat facilisis urna, vitae dictum enim enim nec urna. Suspendisse potenti."
+                        },
+                        step3: {
+                            numberTitle: "03",
+                            title: "Enjoy Your Meals",
+                            content: "Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Nullam id dolor id nibh ultricies vehicula ut id elit. Etiam porta sem malesuada magna mollis euismod."
+                        },
+                        images: {
+                            bigBowl: Images.main.howItWorks.bigBowl,
+                            salad: Images.main.howItWorks.salad,
+                            womaneat: Images.main.howItWorks.womaneat,
+                        }
+                    }
+                },
+                benefits: { 
+                    title: "Benefits", 
+                    subtitle: "Get meal plans tailored to your unique dietary needs, preferences, and goals, ensuring a balanced and enjoyable diet.",
+                    content: { 
+                        benefit1: {
+                            title: "Personalized Nutrition",
+                            description: "Get meal plans tailored to your unique dietary needs, preferences, and goals, ensuring a balanced and enjoyable diet."
+                        },
+                        benefit2: {
+                            title: "Cost-Effective Shopping",
+                            description: "Reduce food waste and save money with efficient grocery shopping lists that align perfectly with your meal plans."
+                        },
+                        benefit3: {
+                            title: "Seamless Grocery Delivery",
+                            description: "Enjoy the convenience of having all your meal ingredients delivered right to your doorstep through our local grocery store partnerships."
+                        },
+                        benefit4: {
+                            title: "Time-Saving Convenience",
+                            description: "Say goodbye to meal planning stress. Our AI-driven platform simplifies your weekly meal preparation, saving you valuable time."
+                        },
+                        benefit5: {
+                            title: "Community Support",
+                            description: "Join a community of like-minded individuals, share experiences, recipes, and tips, and get motivated on your journey to healthier eating."
+                        },
+                        benefit6: {
+                            title: "Healthier Eating",
+                            description: "Easily adopt a healthier lifestyle with nutrient-rich meal plans and educational content on nutrition and wellness."
+                        },
+                        images: {
+                            groceryBag: Images.main.benefits.groceryBag,
+                            groceryBag2x: Images.main.benefits.groceryBag2x,
+                            vegetablesBag: Images.main.benefits.vegetablesBag,
+                            vegetablesBag2x: Images.main.benefits.vegetablesBag2x, 
+                        }
+                    }
+                },
+                recipes: { 
+                    title: "Recipes",
+                    subtitle: "Recipes crafted by AI, personalized to perfectly align with your unique dietary needs and flavor preferences",
+                    content: {
+                        savoryQuinoaBowl: {
+                            title: "Savory Quinoa and Roasted Vegetable Bowl"   
+                        },
+                        herbGrilledChickenSalad: {
+                            title: "Herb-Infused Grilled Chicken with Seasonal Greens"
+                        },
+                        lentilKaleSalad: {
+                            title: "Mediterranean Lentil and Kale Salad"
+                        },
+                        images: {
+                            quinoa: Images.main.recipes.quinoa,
+                            quinoa2x: Images.main.recipes.quinoa2x,
+                            herb: Images.main.recipes.herb,
+                            herb2x: Images.main.recipes.herb2x,
+                            lentil: Images.main.recipes.lentil,
+                            lentil2x: Images.main.recipes.lentil2x,
+                        }
+                    }
+                },
+                testimonials: {
+                    title: "Testimonials",
+                    subtitle: "Real stories from satisfied users: see how our platform changes lives",
+                    content: {
+                        testimonial1: {
+                            name: "Emily R.",
+                            feedback: "Since using AIChefMate, I've rediscovered my love for cooking! The meal plans are easy and delicious. It's been a game-changer for my busy lifestyle."
+                        },
+                        testimonial2: {
+                            name: "Mark T.",
+                            feedback: "I never knew healthy eating could be this simple and satisfying. Thanks to AIChefMate, my family enjoys diverse, nutritious meals every day."
+                        },
+                        testimonial3: {
+                            name: "Sarah J.",
+                            feedback: "The grocery savings have been incredible! Plus, every meal feels tailored just for me. I'm eating better and feeling great. A big thumbs up!"
+                        },
+                        testimonial4: {
+                            name: "Rebecca S.",
+                            feedback: "Switching to AIChefMate has been a revelation for my meal prep routine. The AI-generated plans are spot-on with my tastes and nutritional needs, making healthy eating effortless and enjoyable."
+                        },
+                        images: {
+                            emily_r: Images.main.testimonials.emily_r,
+                            mark_t: Images.main.testimonials.mark_t,
+                            sarah_j: Images.main.testimonials.sarah_j,
+                            rebecca_s: Images.main.testimonials.rebecca_s,
+                            emily_r2x: Images.main.testimonials.emily_r2x,
+                            mark_t2x: Images.main.testimonials.mark_t2x,
+                            sarah_j2x: Images.main.testimonials.sarah_j2x,
+                            rebecca_s2x: Images.main.testimonials.rebecca_s2x,
+                            feddbackIcon: Images.main.testimonials.feddbackIcon,
+                            feddbackIconAlt: Images.main.testimonials.feddbackIconAlt,
+                        }
+                    }
+                },
+                pricing: {
+                    title: "Pricing",
+                    subtitle: "Find your perfect plan: tailored pricing for every need",
+                    content: {
+                        plan1: {
+                            title: "Free Plan",
+                            price: "$0",
+                            features: [
+                                "Basic Ai meal planning",
+                                "Limited recipe access",
+                                "Manual grocery list creation"
+                            ],
+                            bestFor: "Those who want to explore the platform's basic functionalities.",
+                            //textButton: "Get Started For Free"
+                        },
+                        plan2: {
+                            title: "Standard Plan",
+                            price: "$9.99",
+                            features: [
+                                "Comprehensive AI meal planning",
+                                "Full recipe library access",
+                                "Automated grocery lists",
+                                "Grocery delivery integration"
+                            ],
+                            bestFor: "Individuals or families looking for full meal planning support and convenience.",
+                            //textButton: "Subscribe Now"
+                        },
+                        plan3: {
+                            title: "Premium Plan",
+                            price: "$19.99",
+                            features: [
+                                "All Standard features",
+                                "Exclusive gourmet recipes",
+                                "Nutritional consultations",
+                                "Advanced customization options"
+                            ],
+                            bestFor: "Users seeking a premium culinary experience with added personalization and expert guidance.",
+                            //textButton: "Get Premium"
+                        },
+                        images: {
+                            pricing: Images.main.pricing.pricing,
+                            pricing2x: Images.main.pricing.pricing2x,
+                        }
+                    }
+                },
+                healthyInsights: {
+                    title: "Healthy Insights",
+                    subtitle: "Latest News, Expert Blogs, and Nutritional Learning",
+                    content: {
+                        articles: [
+                            {
+                                title: "Mastering Meal Prep: Time-Saving Tips for Busy Weekdays",
+                                image: Images.main.healthyInsights.mealprep,
+                                image2x: Images.main.healthyInsights.mealprep2x,
+                            },
+                            {
+                                title: "The Power of Plant-Based: Benefits Beyond the Plate",
+                                image: Images.main.healthyInsights.plantBased,
+                                image2x: Images.main.healthyInsights.plantBased2x,
+                            },
+                            {
+                                title: "Balancing Nutrients: A Guide to a Well-Rounded Diet",
+                                image: Images.main.healthyInsights.nutrients,
+                                image2x: Images.main.healthyInsights.nutrients2x,
+                            }
+                        ]
+                    },
+                },              
+                faq: {
+                    title: "Frequently Asked Questions",
+                    subtitle: "Meal planning made easy: your questions, answered",
+                    content: {
+                        question1: {
+                            question: "How does the AI personalize my meal plans?",     
+                            answer: "Our AI system analyzes your dietary preferences, health goals, and budget to curate meal plans that are unique to you. It continually learns from your feedback and choices to improve its suggestions over time."
+                        },
+                        question2: {
+                            question: "Can I integrate special dietary requirements into my meal plan?",
+                            answer: "Absolutely! Our platform is designed to accommodate a range of dietary needs, including vegetarian, vegan, gluten-free, and keto options. Just specify your requirements in your profile, and your meal plans will be adjusted accordingly."
+                        },
+                        question3: {
+                            question: "How does the grocery delivery service work?",
+                            answer: "Our grocery delivery service is designed to be seamless and convenient. Once your meal plan is created, you can choose to have all the necessary ingredients delivered to your doorstep. We partner with local grocery stores and delivery services to ensure fresh and high-quality ingredients are available for your meals."
+                        },
+                        question4: {
+                            question: "How does the platform accommodate allergies and food sensitivities?",
+                            answer: "The AI analyzes your dietary preferences, restrictions, and goals to create customized meal plans just for you."
+                        },
+                        question5: {
+                            question: "Are there options for budget-conscious users?",
+                            answer: "Absolutely! Our platform is designed to accommodate a range of dietary needs, including vegetarian, vegan, gluten-free, and keto options. Just specify your requirements in your profile, and your meal plans will be adjusted accordingly."
+                        },
+                        question6: {
+                            question: "Can I adjust my meal plans after they've been created?",
+                            answer: "Our AI takes food allergies into account when creating your meal plan, ensuring your safety and satisfaction."
+                        },
+                        question7: {
+                            question: "How can I access nutrition education and healthy eating tips?",     
+                            answer: "Our platform offers a wealth of resources, including articles, videos, and personalized recommendations to help you make informed food choices and develop healthy eating habits."
+                        },
+                        question8: {
+                            question: "What makes your AI-driven meal planning different from other meal planning services?",
+                            answer: "Our AI-driven meal planning is unique because it offers personalized meal plans that adapt to your specific dietary needs, preferences, and goals. We also provide ongoing support and resources to help you maintain a healthy lifestyle."
+                        }
+                    },
+                },
+                needAssistance: {
+                    title: "Need Assistance?",
+                    subtitle: "We’re here to help!",
+                    content: {  
+                        description: "Contact our support team for any questions, feedback, or technical issues. Your satisfaction is our priority.",
+                        images: {
+                            assistance: Images.main.needAssistance.assistance,
+                            assistance2x: Images.main.needAssistance.assistance2x,
+                        }
+                    }
+                }
             }
         },
         footer: {
-            copyright: "© 2023 AiChefMate. Tous droits réservés.",
-            links: [
-                { text: "Politique de confidentialité", url: "/privacy" },
-                { text: "Conditions d'utilisation", url: "/terms" }
-            ]
+            content: {
+                description: "Join us on our journey to make meal planning simple and joyful. Connect with us on social media, explore our FAQs for quick answers, or drop us a line anytime.",
+                copyright: "© 2064 AIChefMate. All rights reserved. | Privacy Policy | Terms of Use",
+                links: [
+                    { 
+                        facebook: Images.footer.facebook, 
+                        facebook2x: Images.footer.facebook2x, 
+                        url: "https://facebook.com" 
+                    },
+                    { 
+                        twitter: Images.footer.twitter, 
+                        twitter2x: Images.footer.twitter2x, 
+                        url: "https://twitter.com" 
+                    },
+                    { 
+                        tiktok: Images.footer.tiktok, 
+                        tiktok2x: Images.footer.tiktok2x, 
+                        url: "https://tiktok.com" 
+                    }
+                ],
+                altLogo: Images.footer.altLogo,
+                altLogo2x: Images.footer.altLogo2x,
+            },
         },
     }
-    
 }
-    
---
+++ src/scripts/images.js
@@ -0,0 +1,181 @@
+// Navbar images
+import Navlogo from '@assets/images/Navbar/AichefMateLogo1x.png'
+import Navlogo2x from '@assets/images/Navbar/AichefMateLogo2x.png'
+
+// Header images
+import bowl from '@assets/images/Header/bowl.webp'
+import bowl2x from '@assets/images/Header/bowl2x.webp'
+import groceryBag from '@assets/images/Header/grocerybag.webp'
+import groceryBag2x from '@assets/images/Header/grocerybag2x.webp'
+import natureHand from '@assets/images/Header/naturehand.webp'
+import natureHand2x from '@assets/images/Header/naturehand2x.webp'
+import vegetables from '@assets/images/Header/vegetables.webp'
+import vegetables2x from '@assets/images/Header/vegetables2x.webp'
+
+// Header Icons
+import bread from '@assets/images/Header/icons/bread.png'
+import bread2x from '@assets/images/Header/icons/bread2x.png'
+import carrot from '@assets/images/Header/icons/carrot.png'
+import carrot2x from '@assets/images/Header/icons/carrot2x.png'
+import cart from '@assets/images/Header/icons/cart.png'
+import cart2x from '@assets/images/Header/icons/cart2x.png'
+import cherry from '@assets/images/Header/icons/cherry.png'
+import cherry2x from '@assets/images/Header/icons/cherry2x.png'
+
+// Main images
+import demo from '@assets/images/main/demo/demo.webp'
+import demo2x from '@assets/images/main/demo/demo2x.webp'
+import player from '@assets/images/main/demo/player.png'
+import player2x from '@assets/images/main/demo/player2x.png'
+
+import bigBowl from '@assets/images/main/howItWorks/bigbowl.webp'
+import salad from '@assets/images/main/howItWorks/salad.webp'
+import womaneat from '@assets/images/main/howItWorks/womaneat.webp'
+
+import groceryBagBenefits from '@assets/images/main/benefits/grocerybag.webp'
+import groceryBagBenefits2x from '@assets/images/main/benefits/grocerybag2x.webp'
+import vegetablesBag from '@assets/images/main/benefits/vegetables.webp'
+import vegetablesBag2x from '@assets/images/main/benefits/vegetables2x.webp'
+
+
+import quinoa from '@assets/images/main/recipes/quinoa.png'
+import quinoa2x from '@assets/images/main/recipes/quinoa2x.png'
+import herb from '@assets/images/main/recipes/herb.png'
+import herb2x from '@assets/images/main/recipes/herb2x.png'
+import lentil from '@assets/images/main/recipes/lentil.png'
+import lentil2x from '@assets/images/main/recipes/lentil2x.png'
+
+import emily_r from '@assets/images/main/testimonials/emily_r.png'
+import emily_r2x from '@assets/images/main/testimonials/emily_r2x.png'
+import mark_t from '@assets/images/main/testimonials/mark_t.png'
+import mark_t2x from '@assets/images/main/testimonials/mark_t2x.png'
+import sarah_j from '@assets/images/main/testimonials/sarah_j.png'
+import sarah_j2x from '@assets/images/main/testimonials/sarah_j2x.png'
+import rebecca_s from '@assets/images/main/testimonials/rebbeca_s.png'
+import rebecca_s2x from '@assets/images/main/testimonials/rebbeca_s2x.png'
+import feddbackIcon from '@assets/images/main/testimonials/feedbackIcon.png'
+import feddbackIconAlt from '@assets/images/main/testimonials/feedbackIconAlt.png'
+
+import pricing from '@assets/images/main/pricing/pricing.webp'
+import pricing2x from '@assets/images/main/pricing/pricing2x.webp'
+
+import mealprep from '@assets/images/main/healthyInsights/mealprep.webp'
+import mealprep2x from '@assets/images/main/healthyInsights/mealprep2x.webp'
+import nutrients from '@assets/images/main/healthyInsights/nutrients.webp'
+import nutrients2x from '@assets/images/main/healthyInsights/nutrients2x.webp'
+import plantBased from '@assets/images/main/healthyInsights/plantbased.webp'
+import plantBased2x from '@assets/images/main/healthyInsights/plantbased2x.webp'
+
+import assistance from '@assets/images/main/needAssistance/assistance.webp'
+import assistance2x from '@assets/images/main/needAssistance/assistance2x.webp'
+
+// Footer images
+import altLogo from '@assets/images/footer/altLogo.png'
+import altLogo2x from '@assets/images/footer/altLogo2x.png'
+import facebook from '@assets/images/footer/fb.png'
+import facebook2x from '@assets/images/footer/fb2x.png'
+import twitter from '@assets/images/footer/twitter.png'
+import twitter2x from '@assets/images/footer/twitter2x.png'
+import tiktok from '@assets/images/footer/tiktok.png'
+import tiktok2x from '@assets/images/footer/tiktok2x.png'
+
+// Ajoute ici les autres images si besoin
+
+export const Images = {
+  // Navbar
+  navbar: {
+    logo: Navlogo,
+    logo2x: Navlogo2x,
+  },
+
+  // Header
+  header: {
+    bowl,
+    bowl2x,
+    groceryBag,
+    groceryBag2x,
+    natureHand,
+    natureHand2x,
+    vegetables,
+    vegetables2x,
+    icons: {
+      bread,
+      bread2x,
+      carrot,
+      carrot2x,
+      cart,
+      cart2x,
+      cherry,
+      cherry2x,
+    },
+  },
+
+  // Main
+  main: {
+    demo: {
+      demo,
+      demo2x,
+      player,
+      player2x,
+    },
+    howItWorks: {
+      bigBowl,
+      salad,
+      womaneat,
+    },
+    benefits: {
+      groceryBag: groceryBagBenefits,
+      groceryBag2x: groceryBagBenefits2x,
+      vegetablesBag: vegetablesBag,
+      vegetablesBag2x: vegetablesBag2x,
+    },
+    recipes: {
+      quinoa,
+      quinoa2x,
+      herb,
+      herb2x,
+      lentil,
+      lentil2x,
+    },
+    testimonials: {
+      emily_r,
+      emily_r2x,
+      mark_t,
+      mark_t2x,
+      sarah_j,
+      sarah_j2x,
+      rebecca_s,
+      rebecca_s2x,
+      feddbackIcon,
+      feddbackIconAlt,
+    },
+    pricing: {
+      pricing,
+      pricing2x,
+    },
+    healthyInsights: {
+      mealprep,
+      mealprep2x,
+      nutrients,
+      nutrients2x,
+      plantBased,
+      plantBased2x,
+    },
+    needAssistance: {
+      assistance,
+      assistance2x,
+    },
+  },
+
+  // Footer
+  footer: {
+    altLogo,
+    altLogo2x,
+    facebook,
+    facebook2x,
+    twitter,
+    twitter2x,
+    tiktok,
+    tiktok2x,
+  },
+}

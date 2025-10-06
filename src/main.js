d96a4731610cb76ecca68a798170d30e3ca6ea54
Pierre NANGI <pierrenangidev>
2025-10-06 07:18:18 +0200

    Ubuntu setup workspace fixed

--- src/main.js
+++ src/main.js
@@ -1,17 +1,12 @@
 // Importation de la fonction pour créer l'app Vue et du composant racine
 import { createApp } from 'vue'
-import App from '@/App.vue'
+import App from './App.vue'
 
 // Importation des styles globaux Stylus pour toute l'application
-import '@/stylesheets/App.styl'
+import './stylesheets/App.styl'
 
 // Importation de Font Awesome et des icônes utilisées
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUser, faCoffee } from '@fortawesome/free-solid-svg-icons'
-
-// Ajout des icônes à la bibliothèque Font Awesome
-library.add(faUser, faCoffee)
 
 // Création de l'application Vue
 const app = createApp(App)
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
 import vueDevtools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
   plugins: [
     vue(),
     vueDevtools()
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  ]
 })
